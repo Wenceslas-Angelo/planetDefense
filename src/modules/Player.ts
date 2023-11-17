b432958b951bcfa44cb61b675ceb9895636ad851
Wenceslas-Angelo <wenceslasangelo@gmail.com>
2023-11-17 08:15:28 +0300

    canvas rotation

--- src/modules/Player.ts
+++ src/modules/Player.ts
@@ -10,6 +10,7 @@ class Player {
   private image: HTMLImageElement;
   game: Game;
   private aim: number[];
+  private angle: number;
 
   constructor(game: Game) {
     this.game = game;
@@ -18,6 +19,7 @@ class Player {
     this.radius = 40;
     this.image = createImage(playerImage);
     this.aim = [];
+    this.angle = 0;
   }
 
   update() {
@@ -27,14 +29,19 @@ class Player {
     this.aim = calcAim(this.game.mouse, { x: planetX, y: planetY });
     this.x = planetX + (planetRadius + this.radius) * this.aim[0];
     this.y = planetY + (planetRadius + this.radius) * this.aim[1];
+    this.angle = Math.atan2(-this.aim[3], -this.aim[2]);
   }
 
   draw(context: CanvasRenderingContext2D) {
-    context.drawImage(this.image, this.x - this.radius, this.y - this.radius);
+    context.save();
+    context.translate(this.x, this.y);
+    context.rotate(this.angle);
+    context.drawImage(this.image, -this.radius, -this.radius);
     context.beginPath();
-    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    context.arc(0, 0, this.radius, 0, Math.PI * 2);
     context.stroke();
     context.strokeStyle = "white";
+    context.restore();
   }
 }
 
